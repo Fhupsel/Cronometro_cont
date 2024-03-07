f8b15cb6ac97f05021845276be9c9831869aef9d
Fhupsel <fmhupsel@outlook.com>
2024-03-07 13:57:38 -0300

    up

--- script.js
+++ script.js
@@ -3,7 +3,16 @@ import countdown from "./countdown.js";
 let dateObject;
 document.getElementById("getDateButton").addEventListener("click", function() {
   const inputDate = document.getElementById("dateInput").value;
-  const dateObject = new Date(inputDate + "T00:00:00");
+  dateObject = new Date(inputDate + "T00:00:00");
+  const tempo = new countdown(dateObject);
+  const final = tempo.final;
+  const days = document.getElementById("days");
+  days.innerText += final.days + " dias, ";
+  const hours = document.getElementById("hours");
+  hours.innerText +=final.hours +":";
+  const minutes = document.getElementById("minutes");
+  minutes.innerText +=final.minutes +":";
+  const seconds = document.getElementById("seconds");
+  seconds.innerText +=final.seconds;
 });
 
-console.log(dateObject)
